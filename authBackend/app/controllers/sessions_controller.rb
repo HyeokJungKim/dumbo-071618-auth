ff7f5a843dfbf6bd984e3ceb8f252bc3199d5424
Eric Kim <hyeokjungkim@gmail.com>
2018-10-18 13:39:28 -0400

    solution

---
+++ authBackend/app/controllers/sessions_controller.rb
@@ -0,0 +1,44 @@
+class SessionsController < ApplicationController
+  def login
+    @user = User.find_by(username: user_params[:username])
+    if(@user && @user.authenticate(user_params[:password]))
+      token = JWT.encode({user_id: @user.id}, "071618")
+      render json: {
+        user: {user_id: token, username: @user.username},
+        snacks: @user.snacks
+      }
+    else
+      render json: {
+        error: "Invalid Username or Password"
+      }, status: :unauthorized
+    end
+  end
+
+
+  def persist
+    token = request.headers["Authorization"]
+    begin
+      payload = JWT.decode(token, "071618", true)
+    rescue JWT::DecodeError
+      nil
+    end
+    if(payload)
+      id = payload[0]["user_id"]
+      @user = User.find(id)
+      render json: {
+        user: {user_id: @user.id, username: @user.username},
+        snacks: @user.snacks
+      }
+    else
+      render json: {
+        error: "Invalid token"
+      }
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- authBackend/config/routes.rb
+++ authBackend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :snacks, only: [:index]
   resources :users, only: [:show, :create]
+  
+  post "/login", to: "sessions#login"
+  get "/persist", to: "sessions#persist"
 end
